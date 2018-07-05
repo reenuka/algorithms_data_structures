b39a12f1efa800c6c189f5da644ac42f2b11de97
Reenuka Sarika Rāmphal <reenuka@gmail.com>
2018-07-04 21:05:16 -0700

    algorithms

---
+++ hackerrank/algorithms/minimumAbsDiff.js
@@ -0,0 +1,12 @@
+// Given an array of integers, find and print the minimum absolute difference between any two elements in the array.
+// I: array of integers
+// O: number representing largest absolute difference between any two elements in the array
+// C: number of integers in array > 2 and < 10,000, values are > -10,000,000,000 and < 10,000,000,000
+// E: none?
+
+function minimumAbsoluteDifference (arr) {
+    // declare variable to track minimum difference
+    // iterate over array
+        // 
+    // return minimum difference
+}
